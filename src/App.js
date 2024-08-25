a085588bf43371c12dc7bfb369baac5540f0b71f
Kishore K <kishorekarthik2003@gmail.com>
2024-08-25 23:46:23 +0530

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Form } from "./form";
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [currentForm, setCurrentForm] = useState('Form');
+
+  useEffect(() => {
+    fetch('http://localhost:9090/books')
+        .then(response => response.json())
+        .then(result => setBooks(result))
+        .catch(error => console.error('Error:', error));
+  }, []);
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  const addBook = (book) => {
+    setBooks([...books, book]); // Add the new book to the list
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        currentForm === "Form" ? 
+          <Form onFormSwitch={toggleForm} onBookAdded={addBook} /> : null
+      }
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title} by {book.author}</li>
+        ))}
+      </ul>
     </div>
   );
 }
